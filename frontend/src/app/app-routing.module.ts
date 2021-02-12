b3f1b1a7f23aa1103b9db2191d0602e594ea0958
Nolan Guzman <nolanguzman@Nolans-Mac-mini.local>
2021-02-12 11:48:14 -0800

    pushing frontend code. Frontend now communicates with express backend and can send and recieve get requests. Support for multiple chemicals and multiple conversion types

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConversionsComponent } from './conversions/conversions.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: ':rep', component: ConversionsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConversionsComponent } from './conversions/conversions.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConversionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
